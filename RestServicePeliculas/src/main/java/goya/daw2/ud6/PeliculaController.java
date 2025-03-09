8d2c216e067ec2d0a8a9c380677db77bea8d0a12
Barroso18 <david.barrosomartinez@gmail.com>
2025-03-09 23:53:00 +0100

    Cambios en el controler

--- RestServicePeliculas/src/main/java/goya/daw2/ud6/PeliculaController.java
+++ RestServicePeliculas/src/main/java/goya/daw2/ud6/PeliculaController.java
@@ -1,5 +1,34 @@
 package goya.daw2.ud6;
 
-public class PeliculaController {
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("peliculas")
+@RestController
+public class PeliculaController {
+	private final RepositorioPelicula repositorio;
+	public PeliculaController(RepositorioPelicula repositorio) {
+		this.repositorio = repositorio;
+	}
+	
+	@GetMapping("/")
+	/*List<Pelicula> all() {
+	    return repositorio.findAll();
+	}*/
+	public ResponseEntity<Iterable<Pelicula>> getAllPeliculas() {
+        return ResponseEntity.ok(repositorio.findAll());
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Pelicula> getPeliculaById(@PathVariable("id") Long id) {
+        return repositorio.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
--- RestServicePeliculas/src/main/java/goya/daw2/ud6/RepositorioPelicula.java
+++ RestServicePeliculas/src/main/java/goya/daw2/ud6/RepositorioPelicula.java
@@ -1,13 +1,14 @@
 package goya.daw2.ud6;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface RepositorioPelicula extends JpaRepository<Pelicula,Long>{
 	List<Pelicula> findByNombre(@Param("nombre") String nombre);
+	public Optional<Pelicula> findById(Long id);
 }
