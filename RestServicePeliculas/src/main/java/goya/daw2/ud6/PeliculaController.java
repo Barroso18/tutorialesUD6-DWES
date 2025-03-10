2e3ea7aaac89a5a2b683f88ee8a6bfeb0883d123
Barroso18 <david.barrosomartinez@gmail.com>
2025-03-10 13:55:17 +0100

    terminado

--- RestServicePeliculas/src/main/java/goya/daw2/ud6/PeliculaController.java
+++ RestServicePeliculas/src/main/java/goya/daw2/ud6/PeliculaController.java
@@ -1,18 +1,19 @@
 package goya.daw2.ud6;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 
 @RequestMapping("peliculas")
@@ -66,7 +67,23 @@ public class PeliculaController {
             : ResponseEntity.status(HttpStatus.CREATED).body("Película creada con éxito");
 	}
 	
-	
+	@PatchMapping("/{id}")
+    public ResponseEntity<?> updatePelicula(@PathVariable("id") Long id, @RequestBody Pelicula pelicula) {
+        Optional<Pelicula> existingPelicula = repositorio.findById(id);
+        if (!existingPelicula.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        
+        Pelicula updatedPelicula = existingPelicula.get();
+        if (pelicula.getNombre() != null) {
+            if (repositorio.findByNombre(pelicula.getNombre()).isPresent()) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("NOMBRE YA EXISTENTE");
+            }
+            updatedPelicula.setNombre(pelicula.getNombre());
+        }
+        repositorio.save(updatedPelicula);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 	
 	 @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePelicula(@PathVariable("id") Long id) {
